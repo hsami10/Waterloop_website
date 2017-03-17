0b6630f927d8f201f624990d532c4e586797217c
Hassaan Sami <hassaan.sami@live.com>
2017-03-17 18:25:16 -0400

    Add MidFace and EndFace

--- app/src/components/home.jsx
+++ app/src/components/home.jsx
@@ -4,7 +4,6 @@ import NavBar from './components/navbar.jsx';
 import {Grid, Row, Col} from 'react-bootstrap';
 import {Button, ButtonToolbar} from 'react-bootstrap';
 
-
 //navbar component
 //FirstFace: main picture comp
 //MidFace: 'beyond science fiction - watch university video' component
@@ -37,7 +36,7 @@ class FirstFace extends React.Component {
                         </Button>
                         <br />
                         <div>
-                            <img src="../../bin/Images/arrow-down.png"/>
+                            <img src="../../bin/Images/arrow-down.png" alt="Arrow_down" height="50" width="50"/>
                         </div>
                     </Col>
                 </Row>
@@ -47,9 +46,40 @@ class FirstFace extends React.Component {
 }
 
 class MidFace extends React.Component {
-    
+    render() {
+        return (
+            <Grid>
+                <Row>
+                    <Col md={10} lg={10} mdOffset={1} lgOffset={1}>
+                        <div>BEYOND SCIENCE FICTION</div>
+                        <br />
+                        <div>Watch the University Video <span>&rarr;</span></div>
+                        <br />
+                        <div>
+                            <img src="../../bin/Images/arrow-down.png" alt="arrow"/>
+                        </div>
+                    </Col>
+                </Row>
+            </Grid>
+        );
+    }
 }
 
 class EndFace extends React.Component {
-    
+    render() {
+        return (
+            <Grid>
+                <Row>
+                    <Col md={10} lg={10} mdOffset={1} lgOffset={1}>
+                        <div>WHAT IS WATERLOOP?</div>
+                        <br />
+                        <div>Watch the Video <span>&rarr;</span></div>
+                        <div>
+                            <img src="../../bin/Images/arrow-down.png" alt="arrow"/>
+                        </div>
+                    </Col>
+                </Row>
+            </Grid>
+        );
+    }
 }
