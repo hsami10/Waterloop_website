52be8862505837caff4045fe40bc4372ad5bb18d
Hassaan Sami <hassaan.sami@live.com>
2017-03-19 10:56:52 -0400

    Add action plan for Team page, as comments.

--- app/src/components/Team.jsx
+++ app/src/components/Team.jsx
@@ -1,1 +1,11 @@
 import React from 'react';
+
+/*
+- make a div that contains the navbar and the OUR TEAM div
+- then Jake's Captain div
+- then a div containing all the team members:
+- use array of objects, each containing name, title and image url. 
+- map through the array. Inside function, call the Item component which renders a box for each member
+- the Item component renders a Bootstrap Divider Thumbnail for each person
+*/
+
