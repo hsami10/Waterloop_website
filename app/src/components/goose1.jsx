ab0b6d89f8f7f11413a0c639a5adbad0548af0a0
Hassaan Sami <hassaan.sami@live.com>
2017-03-20 13:43:53 -0400

    Add the rest of the headings and content in Goose2

--- app/src/components/goose1.jsx
+++ app/src/components/goose1.jsx
@@ -20,6 +20,63 @@ export default class Goose1 extends React.Component {
                         veritus. Quot unum efficiantur ex eum. Meis summo repudiare ius te, cu ignota doctus 
                         consetetur duo. Cu per labores eligendi neglegentur, eam in denique suscipit convenire.</p>
                 </Row>
+                <Row>
+                    <img src='http://placehold.it/650x650' />
+                    <h2>POD SHELL</h2>
+                    <p>Lorem ipsum dolor sit amet, mei dicunt recusabo temporibus ei, mel deseruisse sententiae 
+                        incorrupte no. Ut nonumes deserunt nec, duo ea causae utroque partiendo, ne ius graece 
+                        veritus. Quot unum efficiantur ex eum. Meis summo repudiare ius te, cu ignota doctus 
+                        consetetur duo. Cu per labores eligendi neglegentur, eam in denique suscipit convenire.</p>
+                </Row>
+                <Row>
+                    <img src='http://placehold.it/650x650' />
+                    <h2>LEVITATION SYSTEM</h2>
+                    <img src='http://placehold.it/650x650'/>
+                    <p>Lorem ipsum dolor sit amet, mei dicunt recusabo temporibus ei, mel deseruisse sententiae 
+                        incorrupte no. Ut nonumes deserunt nec, duo ea causae utroque partiendo, ne ius graece 
+                        veritus. Quot unum efficiantur ex eum. Meis summo repudiare ius te, cu ignota doctus 
+                        consetetur duo. Cu per labores eligendi neglegentur, eam in denique suscipit convenire.</p>
+                </Row>
+                <Row>
+                    <h2>BRAKING</h2>
+                    <p>Lorem ipsum dolor sit amet, mei dicunt recusabo temporibus ei, mel deseruisse sententiae 
+                        incorrupte no. Ut nonumes deserunt nec, duo ea causae utroque partiendo, ne ius graece 
+                        veritus. Quot unum efficiantur ex eum. Meis summo repudiare ius te, cu ignota doctus 
+                        consetetur duo. Cu per labores eligendi neglegentur, eam in denique suscipit convenire.</p>
+                    <img src='http://placehold.it/650x650'/> 
+                    <h3>EDDY CURRENT BRAKING</h3>
+                    <p>Lorem ipsum dolor sit amet, mei dicunt recusabo temporibus ei, mel deseruisse sententiae 
+                        incorrupte no. Ut nonumes deserunt nec, duo ea causae utroque partiendo, ne ius graece 
+                        veritus. Quot unum efficiantur ex eum. Meis summo repudiare ius te, cu ignota doctus.</p>
+                    <img src='http://placehold.it/650x650'/> 
+                    <h3>FRICTION BRAKING</h3>
+                    <p>Lorem ipsum dolor sit amet, mei dicunt recusabo temporibus ei, mel deseruisse sententiae 
+                        incorrupte no. Ut nonumes deserunt nec, duo ea causae utroque partiendo, ne ius graece.</p>                        
+                    <img src='http://placehold.it/650x650'/>
+                </Row>
+                <Row>
+                    <h2>LATERAL CONTROL</h2>
+                    <p>Lorem ipsum dolor sit amet, mei dicunt recusabo temporibus ei, mel deseruisse sententiae 
+                        incorrupte no. Ut nonumes deserunt nec, duo ea causae utroque partiendo, ne ius graece.</p>                        
+                    <img src='http://placehold.it/650x650'/>
+                </Row>
+                <Row>
+                    <h2>DRIVE TRAIN</h2>
+                    <p>Lorem ipsum dolor sit amet, mei dicunt recusabo temporibus ei, mel deseruisse sententiae 
+                        incorrupte no. Ut nonumes deserunt nec, duo ea causae utroque partiendo, ne ius graece.</p>                        
+                    <img src='http://placehold.it/650x650'/>
+                </Row>
+                <Row>
+                    <h2>ELECTRICAL AND EMBEDDED SYSTEMS</h2>
+                    <h3>ELECTRICAL</h3>
+                    <p>Lorem ipsum dolor sit amet, mei dicunt recusabo temporibus ei, mel deseruisse sententiae 
+                        incorrupte no. Ut nonumes deserunt nec, duo ea causae utroque partiendo, ne ius graece.</p>                        
+                    <h3>EMBEDDED</h3>
+                    <p>Lorem ipsum dolor sit amet, mei dicunt recusabo temporibus ei, mel deseruisse sententiae 
+                        incorrupte no. Ut nonumes deserunt nec, duo ea causae utroque partiendo, ne ius graece.
+                        sententiae incorrupte no. Ut nonumes deserunt nec, duo ea causae utroque partiendo, ne ius graece.</p>
+                    <img src='http://placehold.it/650x650'/>
+                </Row>
             </Grid>
         );
     }
