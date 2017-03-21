de892e3560bf5755ce5ab3580e29514586a24a21
Hassaan Sami <hassaan.sami@live.com>
2017-03-20 20:45:49 -0400

    Add styling to GooseX page. Styling is similar to Goose1 page.

--- app/src/components/gooseX.jsx
+++ app/src/components/gooseX.jsx
@@ -8,13 +8,13 @@ export default class GooseX extends React.Component {
     render() {
         return (
             <Grid>
-                <Row id="g2Backgrnd">
+                <Row id="gXBackgrnd">
                     <NavBar />
                 </Row>
-                <Row id="g2TitleRow">
-                    <h1 id="g2Title">FUTURE: GOOSE X</h1>
+                <Row id="gXTitleRow">
+                    <h1 id="gXTitle">FUTURE: GOOSE X</h1>
                 </Row>
-                <Row id="g2Content">
+                <Row id="gXContent">
                     <Row>
                         <p>Lorem ipsum dolor sit amet, mei dicunt recusabo temporibus ei, mel deseruisse sententiae
                         incorrupte no. Ut nonumes deserunt nec, duo ea causae utroque partiendo, ne ius graece
@@ -32,7 +32,6 @@ export default class GooseX extends React.Component {
                     <Row>
                         <Image src='http://placehold.it/900x600' responsive/>
                         <h2>INTERIOR DESIGN</h2>
-                        <Image src='http://placehold.it/900x600' responsive/>
                         <p>Lorem ipsum dolor sit amet, mei dicunt recusabo temporibus ei, mel deseruisse sententiae
                         incorrupte no. Ut nonumes deserunt nec, duo ea causae utroque partiendo, ne ius graece
                         veritus. Quot unum efficiantur ex eum. Meis summo repudiare ius te, cu ignota doctus
