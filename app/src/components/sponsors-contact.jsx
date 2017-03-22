ea72b63bf46b547489c8c23a60c9f53dbd217ad3
Hassaan Sami <hassaan.sami@live.com>
2017-03-21 20:59:13 -0400

    Add second form for joining team.

--- app/src/components/sponsors-contact.jsx
+++ app/src/components/sponsors-contact.jsx
@@ -3,7 +3,7 @@ import NavBar from './navbar.jsx';
 //BOOTSTRAP IMPORTS
 import { Grid, Row, Col } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
-import { Form, FormControl, ControlLabel, FormGroup, HelpBlock } from 'react-bootstrap';
+import { Form, FormControl, ControlLabel, FormGroup, HelpBlock, Checkbox } from 'react-bootstrap';
 
 export class Sponsors extends React.Component {
     render() {
@@ -24,6 +24,8 @@ export class Sponsors extends React.Component {
     }
 }
 
+let currentTerm = 'Winter 17\'';
+
 export class Contact extends React.Component {
     render() {
         return (
@@ -37,16 +39,17 @@ export class Contact extends React.Component {
                 <Row id="contContent">
                     <Row>
                         <h2>CONTACT US</h2>
-                        <h4>Please complete the form below</h4>
+                        <h4>Please complete the form below</h4><br />
                     </Row>
                     <Row>
                         <form>
+                            <ControlLabel>Name *</ControlLabel>
                             <Form inline>
                                 <FormGroup controlId="fmNameFirst">
-                                    <ControlLabel>Name *</ControlLabel>
                                     <FormControl type="text" />
                                     <HelpBlock>First Name</HelpBlock>
                                 </FormGroup>
+                                {' '}
                                 <FormGroup controlId="fmNameLast">
                                     <FormControl type="text" />
                                     <HelpBlock>Last Name</HelpBlock>
@@ -69,6 +72,70 @@ export class Contact extends React.Component {
                             </FormGroup>
 
                             <Button className="fmSubmit">SUBMIT</Button>
+                        </form>
+                    </Row>
+
+                    <Row>
+                        <h2>JOIN THE TEAM</h2>
+                        <h4>Interested in joining the team? Get in touch below!</h4><br />
+                    </Row>
+
+                    <Row>
+                        <form>
+                            <ControlLabel>Name *</ControlLabel>
+                            <Form inline>
+                                <FormGroup>
+                                    <FormControl type="text" />
+                                    <HelpBlock>First Name</HelpBlock>
+                                </FormGroup>
+                                {' '}
+                                <FormGroup>
+                                    <FormControl type="text" />
+                                    <HelpBlock>Last Name</HelpBlock>
+                                </FormGroup>
+                            </Form>
+
+                            <FormGroup>
+                                <ControlLabel>Email Address *</ControlLabel>
+                                <FormControl type="email" />
+                            </FormGroup>
+
+                            <FormGroup>
+                                <ControlLabel>Resume *</ControlLabel>
+                                <HelpBlock>Attach a link to your resume</HelpBlock>
+                                <FormControl type="text" />
+                            </FormGroup>
+
+                            <FormGroup>
+                                <ControlLabel>Interests *</ControlLabel>
+                                <HelpBlock>Which teams would you be interested in joining?</HelpBlock>
+                                <Checkbox>Sponsorship</Checkbox>
+                                <Checkbox>Media</Checkbox>
+                                <Checkbox>Marketing</Checkbox>
+                                <Checkbox>Logistics</Checkbox>
+                                <Checkbox>Finance</Checkbox>
+                                <Checkbox>Web</Checkbox>
+                                <Checkbox>Levitation</Checkbox>
+                                <Checkbox>Braking</Checkbox>
+                                <Checkbox>Shell</Checkbox>
+                                <Checkbox>Frame</Checkbox>
+                                <Checkbox>Lateral</Checkbox>
+                                <Checkbox>Software</Checkbox>
+                                <Checkbox>Electrical</Checkbox>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <ControlLabel>Term *</ControlLabel>
+                                <HelpBlock>Which term are you available?</HelpBlock>
+                                <FormControl componentClass="select" placeholder={currentTerm}>
+                                    <option>{currentTerm}</option>
+                                    <option>Summer 17'</option>
+                                    <option>Fall 17'</option>
+                                    <option>Winter 18'</option>
+                                </FormControl>
+                            </FormGroup>
+
+                            <Button classNam="fmSubmit">SUBMIT</Button>
                         </form>
                     </Row>
                 </Row>
