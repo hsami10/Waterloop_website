1b537b81dadb55daf8fb7dca58bb824ad96d9bdf
Hassaan Sami <hassaan.sami@live.com>
2017-03-27 18:13:36 -0400

    Give gX the alternating structure.

--- app/src/components/gooseX.jsx
+++ app/src/components/gooseX.jsx
@@ -13,31 +13,45 @@ export default class GooseX extends React.Component {
                 <Row id="gXBackgrnd">
                     <NavBar />
                 </Row>
+
                 <Row id="gXTitleRow">
                     <h1 id="gXTitle">FUTURE: GOOSE X</h1>
                 </Row>
+
                 <Row id="gXContent">
                     <Row>
-                        <p>Lorem ipsum dolor sit amet, mei dicunt recusabo temporibus ei, mel deseruisse sententiae
-                        incorrupte no. Ut nonumes deserunt nec, duo ea causae utroque partiendo, ne ius graece
-                        veritus. Quot unum efficiantur ex eum. Meis summo repudiare ius te, cu ignota doctus
-                        consetetur duo. Cu per labores eligendi neglegentur, eam in denique suscipit convenire.</p>
+                        <Col md={6} mdOffset={3}>
+                            <p>This is our GOOSE X pod, a conceptual full size Hyperloop vehicle inspired by the
+                            prototype we are building. The full size pod will have a capacity of 26 passengers
+                            per pod, interior diameter of 2 meters, top speed around 1220 km/h and cost around
+                            $500,000 per pod.</p>
+                        </Col>
                     </Row>
                     <Row>
-                        <Image src={images + 'flock-imgs/gX-extDesign.jpg'} responsive/>
-                        <h2>EXTERIOR DESIGN</h2>
-                        <p>Lorem ipsum dolor sit amet, mei dicunt recusabo temporibus ei, mel deseruisse sententiae
-                        incorrupte no. Ut nonumes deserunt nec, duo ea causae utroque partiendo, ne ius graece
-                        veritus. Quot unum efficiantur ex eum. Meis summo repudiare ius te, cu ignota doctus
-                        consetetur duo. Cu per labores eligendi neglegentur, eam in denique suscipit convenire.</p>
+                        <Col md={6}>
+                            <Image src={images + 'flock-imgs/gX-extDesign.jpg'} responsive />
+                        </Col>
+                        <Col md={6}>
+                            <h2>EXTERIOR DESIGN</h2>
+                            <p>The exterior design of the pod highlights its primary design feature: the geodetic
+                            structural frame. The front end of the pod features a funnel like shape which would
+                             be used to collect the small amount of low pressure air in front of the train to
+                             eliminate air drag and supply air for the compressor which supplies the air castors.
+                             The matte black finish departs from contemporary transit imagery and signals in the
+                              new era of Hyperloop transit.</p>
+                        </Col>
                     </Row>
                     <Row>
-                        <Image src={images + 'flock-imgs/gX-intDesign.jpg'} responsive/>
-                        <h2>INTERIOR DESIGN</h2>
-                        <p>Lorem ipsum dolor sit amet, mei dicunt recusabo temporibus ei, mel deseruisse sententiae
-                        incorrupte no. Ut nonumes deserunt nec, duo ea causae utroque partiendo, ne ius graece
-                        veritus. Quot unum efficiantur ex eum. Meis summo repudiare ius te, cu ignota doctus
-                        consetetur duo. Cu per labores eligendi neglegentur, eam in denique suscipit convenire.</p>
+                        <Col md={6} mdPush={6}>
+                            <Image src={images + 'flock-imgs/gX-intDesign.jpg'} responsive />
+                        </Col>
+                        <Col md={6} mdPull={6}>
+                            <h2>INTERIOR DESIGN</h2>
+                            <p>Due to cost efficiency, the diameter of the Hyperloop track must be kept as minimal
+                            as possible. For this reason, the pod design is kept as small as possible, and the
+                            curved geometry, white colour palette, and use of interior features resembling
+                            windows creates the illusion of a much bigger space</p>
+                        </Col>
                     </Row>
                 </Row>
             </Grid>
--- app/src/index.jsx
+++ app/src/index.jsx
@@ -17,7 +17,7 @@ class App extends React.Component {
         super(props);
 
         this.state = {
-            page: 'home-page'
+            page: 'gooseX-page'
         }
     }
 
